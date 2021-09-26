7fab65a09d071d53c2d6f9e19389bda6553130f1
grimechristopher <grimechristopher@gmail.com>
2021-09-25 20:37:26 -0700

    Generic Service

---
+++ frontend/src/services/ApiService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const API_BASE_URL = "http://localhost:8000/api/courses/";
+
+const getAll = (model) => {
+    return axios.get(API_BASE_URL + model + '/');
+};
+
+const get = (model, id) => {
+    return axios.get(API_BASE_URL + model + '/' + id + '/');
+};
+
+const create = (model, data) => {
+    return axios.post(API_BASE_URL + model + '/', data);
+};
+
+const update = (model, id, data) => {
+    return axios.put(API_BASE_URL + model + '/' + id + '/', data);
+};
+
+const remove = (model, id) => {
+    return axios.delete(API_BASE_URL + model + '/' + id +'/');
+};
+
+export default {
+    getAll,
+    get,
+    create,
+    update,
+    remove
+};
